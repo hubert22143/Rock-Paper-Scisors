c62fcd875f73399daf1e44fd0e7c0f2a2ac522de
Hubert Baciak <hubert.baciak@o2.pl>
2023-07-27 16:59:56 +0200

    Overall Changes

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
 const StartGame = document.getElementById('start-game');
 const InitialMenu = document.querySelector('.InitialMenu');
 const UserInterface = document.querySelector('.UserInterface');
-function HideStartgameMenu(){
+const play = document.querySelector('.play');
+const playmenu = document.querySelector('.NavGameMenu');
+const TheActualGameMenu = document.querySelector('.TheActualGameMenu');
+function HideStartGameMenu(){
 InitialMenu.style.display = 'none';
+TheActualGameMenu.style.display = 'none';
 UserInterface.style.display = 'flex';
 }
-StartGame.addEventListener('click', HideStartgameMenu);
-
+StartGame.addEventListener('click', HideStartGameMenu);
+function HideNavGameMenu(){
+    playmenu.style.display = 'none';
+    TheActualGameMenu.style.display = "flex";
+}
+play.addEventListener('click' , HideNavGameMenu);
 });
+
