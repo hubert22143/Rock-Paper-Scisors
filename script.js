c152b987de7c1042a56af8f915cc9d77fa747398
Hubert Baciak <hubert.baciak@o2.pl>
2023-07-27 17:32:31 +0200

    Added PlayerChoice, and BotChoice

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const UserInterface = document.querySelector('.UserInterface');
 const play = document.querySelector('.play');
 const playmenu = document.querySelector('.NavGameMenu');
 const TheActualGameMenu = document.querySelector('.TheActualGameMenu');
+const Playerrock = document.querySelector('.ButtonRock');
+const Playerpaper = document.querySelector('.ButtonPaper');
+const Playerscissors = document.querySelector('.ButtonScissors');
 function HideStartGameMenu(){
 InitialMenu.style.display = 'none';
 TheActualGameMenu.style.display = 'none';
@@ -16,5 +19,36 @@ function HideNavGameMenu(){
     TheActualGameMenu.style.display = "flex";
 }
 play.addEventListener('click' , HideNavGameMenu);
-});
 
+
+function PlayerChoice(choice){
+    console.log('Player chose:', choice);
+    const botChoice = BotChoice();
+    console.log('Bot chose:', botChoice);
+}
+Playerrock.addEventListener('click', ()=>{
+PlayerChoice("rock");
+
+})
+Playerpaper.addEventListener('click', ()=>{
+    PlayerChoice("paper");
+});
+Playerscissors.addEventListener('click', () =>{
+PlayerChoice("scissors");
+
+})
+function BotChoice(){
+    const randomValue = Math.floor(Math.random() * 3) + 1;
+    let botchoice;
+    if(randomValue===1){
+        botchoice="Rock";
+    }else if(randomValue===2){
+        botchoice="Paper";
+    }else{
+        botchoice="Scissors";
+    }
+    return botchoice;
+}
+
+
+});
